559d7dc18dc56829fee14663fd9f1fa631c37a9a
zmelka <jjdlwodud@naver.com>
2022-12-11 17:09:57 +0900

    Question, Answer 엔티티 생성

---
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
